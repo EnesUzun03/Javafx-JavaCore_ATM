cfa5f902039b0d5e9f8ef989bd7f68c31f9a67fd
EnesUzun03 <161275404+EnesUzun03@users.noreply.github.com>
2025-03-26 15:30:39 +0300

    SingletonDBConnection ,H2DB

--- src/main/java/com/enesuzun/ibb_ecodation_javafx/database/SingletonDBConnection.java
+++ src/main/java/com/enesuzun/ibb_ecodation_javafx/database/SingletonDBConnection.java
@@ -15,21 +15,75 @@ public class SingletonDBConnection {
     //singelton design pattern yapısında private çok kullanılır
 
     //database information data
-    private static final String URL="jdbc:h2:./h2db/user_management"+ "AUTO_SERVER=TRUE";
+    private static final String URL = "jdbc:h2:./h2db/user_management";
+    //private static final String URL = "jdbc:h2:./h2db/user_management?" + "AUTO_SERVER=TRUE";
+    //private static final String URL = "jdbc:h2:~/h2db/user_management"; //kök dizin
     private static final String USERNAME="sa";
     private static final String PASSWORD="";
 
+    //Singleton Desing pattern
+    private static SingletonDBConnection instance;
+    //Database Connection
+    private Connection connection;
+
+
     //Parametresiz constructer (NOT : SİNGELTON YAPISI İÇİN PRİVATE OLMASI GEREKLİDİR)
-    //EĞER BİR CONSTRUCTERDA PRİVATE KULLANIYORSAK O CLASS'IN DIŞARIDAN NESNE YARATMASINI ENGELLEMEK İSTİYORUZSUR SOLİD 1
+    //EĞER BİR CONSTRUCTERDA PRİVATE KULLANIYORSAK O CLASS'IN DIŞARIDAN NESNE YARATMASINI ENGELLEMEK İSTİYORUZSUR (SOLİD 1)
     private SingletonDBConnection(){
+        try{
+            //JDBC Yükle (Neolduğuna bak)
+            Class.forName("org.h2.Driver");
+            //bağlantı oluşturmak için yazarız
+            this.connection=DriverManager.getConnection(URL,USERNAME,PASSWORD);
+            System.out.println(SpecialColor.GREEN +"VERİTABANI BAĞLANTISI BASARILI"+SpecialColor.RESET);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println(SpecialColor.RED +"VERİTABANI BAĞLANTISI BASARISIZ"+SpecialColor.RESET);
+            throw new RuntimeException("VERİTABANI BAĞLANTISI BASARISIZ");
+        }
+    }
+
+
+    //Singleton design instance
+    public static synchronized SingletonDBConnection getInstance(){
+        if(instance==null){
+            instance = new SingletonDBConnection();
+        }
+        return instance;
+    }
+    //Bağlantı nesnesini çağırma
+    public Connection getConnection() {
+        return connection;
+    }
+
+    //Veri tabanı bağlantısını kapatma
+    public  static void closeConnection(){
+        if(instance != null && instance.connection != null){
+            try{
+                instance.connection.close();
+                System.out.println(SpecialColor.RED+" Veri tabanı bağlantısı kapatıldı "+SpecialColor.RESET);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    //database test
+
+    public static void main(String[] args) {
 
     }
 
-    //Database Connection
-    private static Connection connection;
 
-    //Method
-    public static Connection getConnection() throws SQLException {
+
+
+
+
+}//END CLASS
+
+
+
+/*public static Connection getConnection() throws SQLException {
         if(connection==null || connection.isClosed()){
             try{
                 connection= DriverManager.getConnection(URL,USERNAME,PASSWORD);
@@ -41,8 +95,4 @@ public class SingletonDBConnection {
             }
         }
         return connection;
-    }
-
-
-
-}//END CLASS
+    }*/
