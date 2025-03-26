9412f86107f1269bd5be5ec07b669ab64285c9fb
EnesUzun03 <161275404+EnesUzun03@users.noreply.github.com>
2025-03-26 17:20:25 +0300

    CRUD _ CREATE

--- src/main/java/com/enesuzun/ibb_ecodation_javafx/dao/UserDAO.java
+++ src/main/java/com/enesuzun/ibb_ecodation_javafx/dao/UserDAO.java
@@ -1,17 +1,40 @@
 package com.enesuzun.ibb_ecodation_javafx.dao;
 
+import com.enesuzun.ibb_ecodation_javafx.database.SingletonDBConnection;
 import com.enesuzun.ibb_ecodation_javafx.dto.UserDTO;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.Optional;
 
 public class UserDAO implements IDaoImplements<UserDTO>{
 
     //Fields
+    private Connection connection;//database ile bağlantı sağlar
+
+    //Parametresiz constructer
+    public UserDAO() {
+        this.connection= SingletonDBConnection.getInstance().getConnection();
+    }
 
 
+    /// //////////////////////////////////////////////////
+    /// CRUD İŞLEMLERİ
+
+    ///Create
     @Override
-    public Optional<UserDTO> create(UserDTO entity) {
+    public Optional<UserDTO> create(UserDTO userDTO) {
+        String sql="INSERT INTO users (username,password,email) VALUES(?,?,?)";
+        try(PreparedStatement preparedStatement=connection.prepareStatement(sql)){
+            preparedStatement.setString(1,userDTO.getUserName());
+            preparedStatement.setString(2,userDTO.getPassword());
+            preparedStatement.setString(3,userDTO.getEmail());
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         return Optional.empty();
     }
 
@@ -19,6 +42,7 @@ public class UserDAO implements IDaoImplements<UserDTO>{
     public List<UserDTO> list() {
         return List.of();
     }
+
 
     @Override
     public Optional<UserDTO> findByName(String name) {
